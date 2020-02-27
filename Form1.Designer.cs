8c4f4db1dd0307571607fd5843c3619292c5aa37
shadowfish07 <38303418+shadowfish07@users.noreply.github.com>
2020-02-27 23:42:01 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Bandizip一键分包安装工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_outPath = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_outName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_extraName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txt_size = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_mainFile = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_bandizipPath = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(367, 464);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.txt_outPath);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.txt_outName);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txt_extraName);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.txt_size);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(359, 435);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "压缩";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(164, 340);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(41, 21);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "...";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 15);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "输出路径";
+            // 
+            // txt_outPath
+            // 
+            this.txt_outPath.Location = new System.Drawing.Point(211, 337);
+            this.txt_outPath.Name = "txt_outPath";
+            this.txt_outPath.Size = new System.Drawing.Size(126, 25);
+            this.txt_outPath.TabIndex = 18;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(311, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(26, 21);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 35);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "开始压缩";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "输出文件名";
+            // 
+            // txt_outName
+            // 
+            this.txt_outName.Location = new System.Drawing.Point(211, 292);
+            this.txt_outName.Name = "txt_outName";
+            this.txt_outName.Size = new System.Drawing.Size(126, 25);
+            this.txt_outName.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "分卷文件后缀";
+            // 
+            // txt_extraName
+            // 
+            this.txt_extraName.Location = new System.Drawing.Point(211, 255);
+            this.txt_extraName.Name = "txt_extraName";
+            this.txt_extraName.Size = new System.Drawing.Size(126, 25);
+            this.txt_extraName.TabIndex = 12;
+            this.txt_extraName.Text = "exe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "分卷大小（100k,100MB...)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "要处理的文件";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(14, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(323, 169);
+            this.listBox1.TabIndex = 9;
+            // 
+            // txt_size
+            // 
+            this.txt_size.Location = new System.Drawing.Point(211, 216);
+            this.txt_size.Name = "txt_size";
+            this.txt_size.Size = new System.Drawing.Size(126, 25);
+            this.txt_size.TabIndex = 8;
+            this.txt_size.Text = "100MB";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.txt_mainFile);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(359, 435);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "解压";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(327, 56);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(26, 21);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "+";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 35);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "开始解压";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(305, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_mainFile
+            // 
+            this.txt_mainFile.Location = new System.Drawing.Point(81, 19);
+            this.txt_mainFile.Name = "txt_mainFile";
+            this.txt_mainFile.Size = new System.Drawing.Size(215, 25);
+            this.txt_mainFile.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "主文件";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "要处理的文件";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(9, 80);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(344, 169);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 480);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Bandizip.exe目录";
+            // 
+            // txt_bandizipPath
+            // 
+            this.txt_bandizipPath.Location = new System.Drawing.Point(151, 474);
+            this.txt_bandizipPath.Name = "txt_bandizipPath";
+            this.txt_bandizipPath.Size = new System.Drawing.Size(178, 25);
+            this.txt_bandizipPath.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(335, 473);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 28);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "选择文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 511);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txt_bandizipPath);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_outName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_extraName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txt_size;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_mainFile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_bandizipPath;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_outPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using IWshRuntimeLibrary;
+
+//https://blog.csdn.net/FairyStepWGL/article/details/52904339
+
+
+namespace Bandizip一键分包安装工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (System.IO.File.Exists("path.txt"))
+            {
+                using (StreamReader streamReader = new StreamReader("path.txt"))
+                {
+                    txt_bandizipPath.Text = streamReader.ReadToEnd();
+                }
+            }
+            if (!(txt_bandizipPath.Text == ""))
+                return;
+            DialogResult dialogResult = startOpenFileDialog("请指定Bandizip.exe", false, "Bandizip|Bandizip.exe", "Bandizip.exe");
+            if (dialogResult == DialogResult.OK)
+                txt_bandizipPath.Text = openFileDialog1.FileName;
+            using (StreamWriter streamWriter = new StreamWriter("path.txt"))
+            {
+                streamWriter.Write(txt_bandizipPath.Text);
+                streamWriter.Flush();
+            }
+            //防止窗体最小化显示
+            TopMost = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = startOpenFileDialog("选择文件",true,"","");
+            if (dialogResult == DialogResult.OK)
+            {
+                foreach (var item in openFileDialog1.FileNames)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            if (listBox1.Items.Count >= 2)
+            {
+                txt_outName.Text = "";
+                txt_outName.Enabled = false;
+            }
+        }
+
+        private DialogResult startOpenFileDialog(string title,bool multiselect,string filter,string fileName)
+        {
+            openFileDialog1.Title = title;
+            openFileDialog1.Multiselect = multiselect;
+            openFileDialog1.Filter = filter;
+            openFileDialog1.FileName = fileName;
+            DialogResult dialogResult = openFileDialog1.ShowDialog();
+            return dialogResult;
+        }
+
+        private void Creatlnk(string shortcutName, string targetPath)
+        {
+            string shortcutPath = string.Format("{0}.lnk", shortcutName);
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);//创建快捷方式对象
+            shortcut.TargetPath = targetPath;//指定目标路径
+            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);//设置起始位置
+            shortcut.Save();//保存快捷方式
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!System.IO.File.Exists(Path.GetFileName(txt_bandizipPath.Text)+Path.GetExtension(txt_bandizipPath.Text)+".ink"))
+            {
+                Creatlnk("Bandizip.exe", txt_bandizipPath.Text);
+            }
+            foreach (var item in listBox1.Items)
+            {
+                string outname;
+                if (Path.GetExtension(item.ToString())==".exe")
+                    outname = Path.GetFileNameWithoutExtension(item.ToString()) + "_zip";
+                else
+                    outname = txt_outName.Text == "" ? Path.GetFileNameWithoutExtension(item.ToString()) : txt_outName.Text;
+                try
+                {
+                    ProcessStartInfo processStartInfo= new ProcessStartInfo();
+                    processStartInfo.FileName = "Bandizip.exe";
+                    processStartInfo.Arguments = String.Format("c -fmt:{0} -v:{1} \"{2}\" \"{3}\"", txt_extraName.Text, txt_size.Text, txt_outPath.Text+ outname, item.ToString());
+                    processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    Process proc = Process.Start(processStartInfo);
+                    if(proc !=null)
+                    {
+                        proc.WaitForExit();
+
+                        DirectoryInfo directoryInfo = new DirectoryInfo(txt_outPath.Text==""?Environment.CurrentDirectory:txt_outPath.Text);
+                        foreach (FileInfo item2 in directoryInfo.GetFiles())
+                        {
+                            string[] nameParts = item2.Name.Split('.');
+                            if (nameParts.Length != 2)
+                                continue;
+                            if (nameParts[0] == outname && nameParts[1] != "exe" && nameParts[1].StartsWith("e"))
+                            {
+                                item2.MoveTo(item2.FullName+".exe");
+                            }
+                        }
+
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult= startOpenFileDialog("选择文件", false, "exe|*.exe", "");
+            if(dialogResult ==DialogResult.OK)
+            {
+                txt_mainFile.Text = openFileDialog1.FileName;
+                DirectoryInfo directoryInfo = new DirectoryInfo(Environment.CurrentDirectory);
+                foreach (FileInfo item in directoryInfo.GetFiles())
+                {
+                    string[] nameParts = item.Name.Split('.');
+                    if (nameParts.Length != 3)
+                        continue;
+                    if(nameParts[0] == Path.GetFileNameWithoutExtension(txt_mainFile.Text)&&nameParts[1]!="exe"&&nameParts[1].StartsWith("e")&&nameParts[2]=="exe")
+                    {
+                        listBox2.Items.Add(item.Name);
+                    }
+                }
+            }
+        }
+    }
+}
